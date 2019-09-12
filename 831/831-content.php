49534eac9e0b21b3823aa6e730e12d2854a6fe23
foostart.com <foostart.com@gmail.com>
2019-09-12 08:07:13 +0700

    Commit

---
+++ 831/831-content.php
@@ -0,0 +1,52 @@
+<?php
+$url_host = $_SERVER['HTTP_HOST'];
+
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+
+$url_path = $url_host . $matches[1][0];
+
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-831">
+	<div class="container">
+		<div class="row item">
+	        <div class="col-md-6 col-sm-6 col-xs-12">
+	            <h3 class="text-center">Basic Example</h3>
+	            <div class="well">
+	        		<ul class="list-group checked-list-box">
+	                  <li class="list-group-item">Cras justo odio</li>
+	                  <li class="list-group-item" data-checked="true">Dapibus ac facilisis in</li>
+	                  <li class="list-group-item">Morbi leo risus</li>
+	                  <li class="list-group-item">Porta ac consectetur ac</li>
+	                  <li class="list-group-item">Vestibulum at eros</li>
+	                  <li class="list-group-item">Cras justo odio</li>
+	                  <li class="list-group-item">Dapibus ac facilisis in</li>
+	                  <li class="list-group-item">Morbi leo risus</li>
+	                  <li class="list-group-item">Porta ac consectetur ac</li>
+	                  <li class="list-group-item">Vestibulum at eros</li>
+	                </ul>
+	            </div>
+	        </div>
+
+	        <div class="col-md-6 col-sm-6 col-xs-12">
+	            <h3 class="text-center">Colorful Example</h3>
+	            <div class="well">
+	            	<ul id="check-list-box" class="list-group checked-list-box">
+	                  <li class="list-group-item">Cras justo odio</li>
+	                  <li class="list-group-item" data-color="success">Dapibus ac facilisis in</li>
+	                  <li class="list-group-item" data-color="info">Morbi leo risus</li>
+	                  <li class="list-group-item" data-color="warning">Porta ac consectetur ac</li>
+	                  <li class="list-group-item" data-color="danger">Vestibulum at eros</li>
+	                </ul>
+	                <br />
+	                <button class="btn btn-primary col-xs-12" id="get-checked-data">Get Checked Data</button>
+	            </div>
+	            <pre id="display-json"></pre>
+	        </div>
+		</div>
+	</div>
+</div>
---
+++ 831/831.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+
+if (!class_exists('lessc')) {
+    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+    require_once($dir_block . '/libs/lessc.inc.php');
+}
+$less = new lessc;
+$less->compileFile('less/831.less', 'css/831.css');
+?>
+<html lang="en">
+    <head>
+        <title>831</title>
+        <meta charset="utf-8">
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Basic usage demo">
+        <meta name="author" content="David Deutsch">
+        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/831.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <script src="<?php echo $url_path?>/js/jquery-3.1.1.min.js" type="text/javascript"></script>
+        <script src="<?php echo $url_path?>/js/831.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <?php include '../831/831-content.php'; ?>
+    </body>
+</html>
