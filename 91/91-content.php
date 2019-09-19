e25fe3ca025eb74f039031f40280a8593bd591f7
tdc-students <cuongtk@tdc.edu.vn>
2019-09-19 07:53:49 +0700

    Responsive Web

--- 91/91-content.php
+++ 91/91-content.php
@@ -13,18 +13,20 @@
         $less = new lessc;
         $less->compileFile('less/type-91.less', 'css/type-91.css');
         ?>
-        <link href="css/type-91.css" rel="stylesheet" type="text/css"/>
-<link rel="stylesheet" href="../package/css/swiper.min.css">
+        
+	<link href="css/type-91.css" rel="stylesheet" type="text/css"/>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/css/swiper.min.css" rel="stylesheet" type="text/css"/>
+	
         <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
        
     </head>
 
     <body>
         <div class="type-91">
-            <div class="college-box">
+            <div class="container college-box">
                 <div class="course-events">
                     <div class="wrapper clearfix">
-                        <div class="row">
+                        <div class="row no-margin">
                             <div class="our-college-sidebar col-4 col-md-4 col-sm-12 col-xs-12">
                                 <article>
                                     <h3>Library</h3>
@@ -64,15 +66,43 @@
                                             </article>
                                         </li>
                                         </div>
-                                        <div class="swiper-slide">
-                                            haha
-                                        </div>  
-                                          <div class="swiper-slide">
-                                            haha
+                                       <div class="swiper-slide">
+                                        <li style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 1; display: block; z-index: 2;">								<article>
+                                                <span class="date">28<span>Wed 2015</span></span>
+                                                <h3><a href="#">Nam eget dui. Etiam rhoncus</a></h3>
+                                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur </p>
+                                            </article>
+                                            <article>
+                                                <span class="date">28<span>Wed 2015</span></span>
+                                                <h3><a href="#">Vivamus elementum semper nisi</a></h3>
+                                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur </p>
+                                            </article>
+                                            <article>
+                                                <span class="date">28<span>Wed 2015</span></span>
+                                                <h3><a href="#">In enim justo, rhoncus ut</a></h3>
+                                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur </p>
+                                            </article>
+                                        </li>
                                         </div> 
-                                          <div class="swiper-slide">
-                                            haha
-                                        </div> 
+                                         <div class="swiper-slide">
+                                        <li style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 1; display: block; z-index: 2;">								<article>
+                                                <span class="date">28<span>Wed 2015</span></span>
+                                                <h3><a href="#">Nam eget dui. Etiam rhoncus</a></h3>
+                                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur </p>
+                                            </article>
+                                            <article>
+                                                <span class="date">28<span>Wed 2015</span></span>
+                                                <h3><a href="#">Vivamus elementum semper nisi</a></h3>
+                                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur </p>
+                                            </article>
+                                            <article>
+                                                <span class="date">28<span>Wed 2015</span></span>
+                                                <h3><a href="#">In enim justo, rhoncus ut</a></h3>
+                                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur </p>
+                                            </article>
+                                        </li>
+                                        </div>
+                                         
                                         </div>	
                                         				
 
