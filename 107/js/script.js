5c6a99724105cadb460f7890cb4f2a0652cd61a0
leducdung147 <leducdung147@gmail.com>
2019-09-22 21:25:45 +0700

    Fix function select all, add css color

--- 107/js/script.js
+++ 107/js/script.js
@@ -1,13 +1,14 @@
 $(document).ready(() => {
-    let Arr = $("a[data-toggle='collapse']");
-    Arr.click(function() {
+    let Arr = $("a[data-toggle='collapse']")
+    Arr.click(function()  {
         Arr.closest("h2").removeClass("highlight");
         setTimeout(() => {
             if($(this).attr("aria-expanded") =="true"){
-                console.log($(this).attr("aria-expanded"))
+               
                 $(this).closest("h2").addClass("highlight");
             }
-        }, 100);
+        }, 0);
        
     })
 })
+  
